aa7329ef70e96ee34f54eef1e695bb1332c42c8c
RaymondAya-ay <raymond.p.ayaay@gmail.com>
2021-12-13 21:08:35 +0800

    remove some stuff from git ignore

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = User::all();
+        return view('tables.usersTable', ['user' => $user]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('forms.newUser');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $inputs = $request->validate([
+            'name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|min:8|confirmed',
+            'password_confirmation' => 'required|min:8'
+        ]);
+        $inputs['password'] = Hash::make( $request->input('password'));
+        User::create($inputs);
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $user = User::find($id);
+
+        return view('forms.updateUser', ['user' => $user]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function changeAdmin($id){
+        $user = User::find($id);
+        if($user){
+            $user->admin = !$user->admin;
+            $user->save();
+        }
+
+        return redirect()->route('user.index');
+    }
+
+    public function changeBan($id){
+        $user = User::find($id);
+        
+        if($user){
+            $user->isBan = !$user->isBan;
+            $user->save();
+        }
+        
+        return redirect()->route('user.index');
+    }
+
+    public function update(Request $request, $id)
+    {
+       $user = User::find($id);
+        if($user){
+            $inputs = $request->except(['_token', '_method']);
+            $user->update($inputs);
+        }
+        
+        return redirect()->route('user.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('admin')->default(0);
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->integer('isBan')->default('0');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'admin' => true,
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Juan Karlos',
+            'email' => 'juankarlos@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Beyonce',
+            'email' => 'beyonce@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Billy Eyelash',
+            'email' => 'blash@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'Julies Bakeshop',
+            'email' => 'jb@gmail.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
